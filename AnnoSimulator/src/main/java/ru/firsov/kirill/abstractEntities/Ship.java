7de2274101c1afa9ffea31b5454c75b5200d315f
VanGoghDev <kifirigor@gmail.com>
2018-09-09 13:06:53 +0300

    Abstract Classes

---
+++ AnnoSimulator/src/main/java/ru/firsov/kirill/abstractEntities/Ship.java
@@ -0,0 +1,4 @@
+package ru.firsov.kirill.abstractEntities;
+
+public abstract class Ship {
+}
---
+++ AnnoSimulator/src/main/java/ru/firsov/kirill/abstractEntities/Store.java
@@ -0,0 +1,8 @@
+package ru.firsov.kirill.abstractEntities;
+
+public abstract class Store {
+
+    Integer capacity = null;
+
+
+}
