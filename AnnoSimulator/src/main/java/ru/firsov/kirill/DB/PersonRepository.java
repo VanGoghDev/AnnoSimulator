bec270fb6038bdff19977e0640b3ac76638da9e5
VanGoghDev <kifirigor@gmail.com>
2018-09-10 00:37:39 +0300

    Implemented methods of PersonRepository

--- AnnoSimulator/src/main/java/ru/firsov/kirill/DB/PersonRepository.java
+++ AnnoSimulator/src/main/java/ru/firsov/kirill/DB/PersonRepository.java
@@ -1,9 +1,7 @@
 package ru.firsov.kirill.DB;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 
 public class PersonRepository implements IRepository<Person> {
@@ -36,21 +34,81 @@ public class PersonRepository implements IRepository<Person> {
 
     @Override
     public void update(Person entity) {
-
+        String sql = "UPDATE PERSON set NAME = ?, AGE = ?, SALARY = ? WHERE id = ?";
+        try {
+            conn = MyConnection.getConnection();
+            st = conn.prepareStatement(sql);
+            st.setString(1, entity.name);
+            st.setInt(2, entity.age);
+            st.setFloat(3, entity.salary);
+            st.setInt(4, entity.id);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                st.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void delete(int id) {
+        String sql = "DELETE FROM PERSON WHERE id=?";
 
+        try{
+            conn = MyConnection.getConnection();
+            st = conn.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                st.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public Person get(int id) {
-        return null;
+        String sql = "SELECT * FROM PERSON WHERE id=?";
+        Person p = null;
+        try {
+            conn = MyConnection.getConnection();
+            st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                p = new Person(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getFloat(4));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return p;
     }
 
     @Override
     public List<Person> get() {
-        return null;
+        String sql = "SELECT ID, NAME, AGE, SALARY FROM PERSON";
+        List<Person> list = new LinkedList<>();
+        try {
+            conn = MyConnection.getConnection();
+            st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                list.add(new Person(rs.getInt(1),
+                             rs.getString(2),
+                             rs.getInt(3),
+                             rs.getFloat(4)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
