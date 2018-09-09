b90b36fe4b2b3c6be3b368c6e29a261a9d494918
VanGoghDev <kifirigor@gmail.com>
2018-09-09 13:09:42 +0300

    DB

--- AnnoSimulator/src/main/java/ru/firsov/kirill/DB/BDApp.java
+++ AnnoSimulator/src/main/java/ru/firsov/kirill/DB/BDApp.java
@@ -4,9 +4,11 @@ public class BDApp {
 
     public static void main(String[] args) {
         Person p = new Person(11, "Vadim", 21, 40.50f);
+        Person p2 = new Person(12, "Kirill", 21, 23f);
 
         PersonRepository repository = new PersonRepository();
         repository.insert(p);
+        repository.insert(p2);
     }
 
 }
