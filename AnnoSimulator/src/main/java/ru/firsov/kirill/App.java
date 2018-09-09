5620c87c93f5dc2d944162de518e61fffc5f0717
VanGoghDev <kifirigor@gmail.com>
2018-09-09 13:19:20 +0300

    BD

---
+++ AnnoSimulator/src/main/java/ru/firsov/kirill/App.java
@@ -0,0 +1,14 @@
+package ru.firsov.kirill;
+
+import ru.firsov.kirill.DB.DBTest;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        //DBTest dataBase = new DBTest();
+        //dataBase.ConnectDB();
+
+
+    }
+}
--- AnnoSimulator/src/main/java/ru/firsov/kirill/DB/BDApp.java
+++ AnnoSimulator/src/main/java/ru/firsov/kirill/DB/BDApp.java
@@ -3,12 +3,10 @@ package ru.firsov.kirill.DB;
 public class BDApp {
 
     public static void main(String[] args) {
-        Person p = new Person(11, "Vadim", 21, 40.50f);
-        Person p2 = new Person(12, "Kirill", 21, 23f);
+        Person p = new Person(12, "Kirill", 21, 23f);
 
         PersonRepository repository = new PersonRepository();
         repository.insert(p);
-        repository.insert(p2);
     }
 
 }
